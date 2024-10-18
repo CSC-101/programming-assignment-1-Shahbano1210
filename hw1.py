f7f5c97fd7d4354c690bc97d699402fa55a171ad
Shahbano1210 <saurangz@calpoly.edu>
2024-10-18 11:40:48 -0700

    Part 1-3

--- hw1.py
+++ hw1.py
@@ -3,24 +3,40 @@ import data
 # Write your functions for each part in the space below.
 
 # Part 1
-
-
+def vowel_count (word:str)-> int: # takes in a word or words as str and returns the number of vowels in the word(s) as an int
+    charList = list(word)
+    vowels = ["a","A","e", "E", "I","i","O","o","U","u"]
+    count = 0
+    for i in vowels:
+        if i in charList:
+            count = count +1
+    return(count)
 
 
 # Part 2
 
-def short_lists(input_list:list[list[int]]) -> list:
+def short_lists(input_list:list[list[int]]) -> list: # takes in a list of nested lists of int and returns a new list
+    # of only the nested lists of length 2
     newList = []
     for i in range(len(input_list)):
         if len(input_list[i]) == 2:
             newList.append(input_list[i])
     return newList
 
-print (short_lists([[4, 6], [4], [3, 4, 5], [5, 2]]))
-
 
 # Part 3
 
+def ascending_pairs (input_list:list[list[int]]) -> list: # takes in a list of nested lists of int and returns the same
+    # list but any nested lists of length 2 are arranged in ascending order
+    newList = []
+    for i in range(len(input_list)):
+        if len(input_list[i]) == 2 and input_list[i][0] > input_list[i][1]:
+            x = input_list[i][0]
+            y = input_list[i][1]
+            input_list[i][0] = y
+            input_list[i][1] = x
+        newList.append(input_list[i])
+    return newList
 
 # Part 4
 
