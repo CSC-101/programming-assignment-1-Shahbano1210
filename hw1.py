9b8a38efdec423b53c7c03fcd30f2d61a4612e37
Shahbano1210 <saurangz@calpoly.edu>
2024-10-23 21:54:39 -0700

    Part 1-8

--- hw1.py
+++ hw1.py
@@ -40,16 +40,71 @@ def ascending_pairs (input_list:list[list[int]]) -> list: # takes in a list of n
 
 # Part 4
 
+def add_prices (dollars: int, cents: int)-> float: # takes in two integers representing dollars and cents and returns
+    # a concatenated price as a float
+    finalPrice = 0
+    totalCents = 0
+    if cents < 100:
+        finalPrice = dollars + cents/100
+    else:
+        dollars = dollars / 100
+        totalCents = dollars + cents/100
+        finalPrice = totalCents / 100
+    return finalPrice
 
 # Part 5
 
+def rectangle_area(top_left: list, bottom_right: list) -> int: # takes in a rectangle consisting of two Points and
+    # returns the area as an int
+    xRec = bottom_right[0] - top_left[0]
+    yRec = top_left[1] - bottom_right[1]
+    area = xRec * yRec
+    return area
 
 # Part 6
+
+def books_by_author (author:str, books:list[data.Book])->list[data.Book]: # takes in a target author and a list of
+    # books and returns book titles that are only by the target author
+    filteredBooks = []
+    for i in range(len(books)):
+        for i in books.authors:
+            if i == author:
+                filteredBooks.append(books[i].title)
+
+    return filteredBooks
+
+
 
 
 # Part 7
 
+def circle_bound(rect:data.Rectangle) -> data.Circle: # takes in a rectangle in two Points and returns a circle as a
+    # radius and center point
+    boundingCircle = data.Circle((0,0),0.0)
+    rad = data.Circle.radius(0.0)
+    cntrPt = data.Circle.center(0,0)
+    xRec = data.Rectangle.bottom_right.x - data.Rectangle.top_left.x
+    yRec = data.Rectangle.top_left.y - data.Rectangle.bottom_right.y
+    if xRec > yRec:
+        rad = data.Circle.radius(float(xRec))
+    else:
+        rad = data.Circle.radius(float(yRec))
+    cntrPt = data.Circle.center(.5*xRec,.5*yRec)
+    boundingCircle = data.Circle(cntrPt,rad)
+    return boundingCircle
+
+
 
 # Part 8
 
-
+def below_pay_average(list:list[data.Employee]) -> list[str]: # takes in a list of employee names and their salaries
+    # and returns a list of employees who are paid less than the average of all employee pay rates
+    avrgPay = 0
+    names = []
+    for i in range(len(list)):
+        avrgPay = avrgPay + i.Employee.pay_rate
+    avrgPay = avrgPay / len(list)
+    for i in list:
+        if data.Employee.pay_rate < avrgPay:
+            names.append(i.Employee.name)
+    return names
--- hw1_test.py
+++ hw1_test.py
@@ -49,21 +49,79 @@ class TestCases(unittest.TestCase):
 
     # Part 4
 
+    def test_add_prices_1(self):
+        dollars = 20.00
+        cents = 120
+        result = hw1.add_prices(dollars,cents)
+        expected = 21.20
+        self.asserEqual(expected,result)
+
+    def test_add_prices_2(self):
+        dollars = 20.00
+        cents = 60
+        result = hw1.add_prices(dollars,cents)
+        expected = 20.60
+        self.assertEqual(expected,result)
 
     # Part 5
+
+    def test_rectangle_area_1(self):
+        top_left = [2,4]
+        bottom_right = [5,3]
+        result = hw1.rectangle_area(top_left,bottom_right)
+        expected = 3
+        self.assertEqual(expected,result)
+
+    def test_rectangle_area_2(self):
+        top_left = [3,5]
+        bottom_right = [5,3]
+        result = hw1.rectangle_area(top_left,bottom_right)
+        expected = 4
+        self.assertEqual(expected,result)
 
 
     # Part 6
 
+    def test_books_by_author_1(self):
+        book1 = data.Book(["Roald Dahl", "another author"],"Charlie & the Chocolate Factory")
+        book2 = data.Book(["J.K. Rowling","diff author"], "Harry Potter & the Sorcerer's Stone")
+        result = hw1.books_by_author("Roald Dahl",[book1,book2])
+        expected = ["Charlie & the Chocolate Factory"]
+        self.assertEqual(expected,result)
+
+    def test_books_by_author_1(self):
+        book1 = data.Book(["J.K. Rowling", "another author"],"Harry Potter & the Sorcerer's Stone")
+        book2 = data.Book(["Roald Dahl","diff author"], "James & the Giant Peach")
+        result = hw1.books_by_author("Roald Dahl",[book1,book2])
+        expected = ["James & the Giant Peach"]
+        self.assertEqual(expected,result)
 
     # Part 7
 
+    def test_circle_bound_1(self):
+        rect = data.Rectangle((0,6),(4,0))
+        result = hw1.circle_bound(rect)
+        expected = data.Circle((2,3),6.0)
+        self.assertEqual(expected,result)
+
+    # couldn't figure out the bug, so no second test
 
     # Part 8
 
+    def test_below_pay_average_1(self):
+        list = [
+            data.Employee("Sally",18),
+            data.Employee("Henry",15),
+            data.Employee("Ron",20),
+            data.Employee("Sarah",16),
+            data.Employee("Missy",18),
+            data.Employee("Will",19)]
+        result = hw1.below_pay_average(list)
+        expected = ["Henry","Sarah"]
+        self.assertEqual(expected,result)
 
 
-
+   # couldn't figure out the bug, so no second test
 
 if __name__ == '__main__':
     unittest.main()
